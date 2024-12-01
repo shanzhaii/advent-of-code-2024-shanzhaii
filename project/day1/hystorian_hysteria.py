ce061473e885f9cf2c07034d508b899f4680a6a6
Alexandre Riou <alexandre.riou@epfl.ch>
2024-12-01 15:52:37 +0100

    implement part 2

--- project/day1/hystorian_hysteria.py
+++ project/day1/hystorian_hysteria.py
@@ -5,7 +5,12 @@ if __name__ == "__main__":
         list_of_ids = [line.strip("\n").split("   ") for line in lists]
         left_ids = [int(left) for left, right in list_of_ids]
         right_ids = [int(right) for left, right in list_of_ids]
+
+        # part 1
         left_ids.sort()
         right_ids.sort()
         print(f"part 1: {reduce(lambda acc, ids: acc + abs(ids[0] - ids[1]), zip(left_ids, right_ids), 0)}")
 
+        # part 2
+        counts = [(left_id, right_ids.count(left_id)) for left_id in left_ids]
+        print(f"part 2: {reduce(lambda acc, ids: acc + abs(ids[0] * ids[1]), counts, 0)}")
