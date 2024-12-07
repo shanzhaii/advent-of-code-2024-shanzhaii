1402e4205a5921da96b83e1160633f8e575c2260
shanzhaiii <alexandre.riou@epfl.ch>
2024-12-08 00:22:17 +0100

    non functional part 2

--- project/day6/guard_gallivant.py
+++ project/day6/guard_gallivant.py
@@ -7,18 +7,34 @@ def is_valid(pos, grid):
     return 0 <= pos[0] < len(grid) and 0 <= pos[1] < len(grid[0])
 
 
-def iterate_through_path(grid):
-    guard_pos = find_guard(grid)
+def iterate_through_path(grid, guard_pos, previous_positions, root_path=True, direction_index=0):
+    new_positions = {}
+    for k, v in previous_positions.items(): # todo fix
+        new_positions[k] = list(v)
+    previous_positions = new_positions
+    # previous_positions = {k: list(v) for k, v in previous_positions}  # deep copy
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    direction_index = 0
     while is_valid(guard_pos, grid):
+        if guard_pos in previous_positions and direction_index in previous_positions.get(guard_pos):
+            if root_path:
+                raise Exception('Should not ever loop from root')
+            return True
         in_front = guard_pos[0] + directions[direction_index][0], guard_pos[1] + directions[direction_index][1]
         if is_valid(in_front, grid) and grid[in_front[0]][in_front[1]] == '#':
             direction_index = (direction_index + 1) % 4
         else:
-            grid[guard_pos[0]][guard_pos[1]] = 'X'
+            if root_path and is_valid(in_front, grid):
+                modified_grid = [[grid[y][x] for x in range(len(grid[0]))] for y in range(len(grid))]  # deep copy
+                modified_grid[guard_pos[0] + directions[direction_index][0]][guard_pos[1] + directions[direction_index][1]] = '#'
+                if grid[guard_pos[0]][guard_pos[1]] != '^' and iterate_through_path(modified_grid, guard_pos, previous_positions, False, direction_index):
+                    grid[guard_pos[0]][guard_pos[1]] = 'O' # wrong position
+                else:
+                    grid[guard_pos[0]][guard_pos[1]] = 'X'
+            previous_positions.setdefault(guard_pos, []).append(direction_index)
             guard_pos = (guard_pos[0] + directions[direction_index][0], guard_pos[1] + directions[direction_index][1])
-    return grid
+    if root_path:
+        return grid
+    return False
 
 
 def flatten(list_of_list):
@@ -26,7 +42,10 @@ def flatten(list_of_list):
 
 
 if __name__ == "__main__":
-    with open("input", "r", newline='\n') as file:
+    with open("test", "r", newline='\n') as file:
         grid = [list(line.strip()) for line in file.readlines()]
-        grid = iterate_through_path(grid)
-        print(f"part 1: {flatten(grid).count('X')}")
+        initial_pos = find_guard(grid)
+        grid = iterate_through_path(grid, initial_pos, {})
+        print(f"part 1: {flatten(grid).count('X') + flatten(grid).count('O')}")
+
+        print(f"part 2: {flatten(grid).count('O')}")
