7b75e1987c62c4c94a8a7195867ef615b5543182
shanzhaiii <alexandre.riou@epfl.ch>
2024-12-08 14:47:22 +0100

    implement day 7 part 2

--- project/day7/bridge_repair.py
+++ project/day7/bridge_repair.py
@@ -1,20 +1,17 @@
 from functools import reduce
-
+from operator import add, mul
 
 def flatten(list_of_list):
     return [elem for array in list_of_list for elem in array]
 
+def concatenation(a, b):
+    return int(str(a) + str(b))
 
-def valid(test_value, equation):
+def valid(test_value, equation, operators):
     possible_sums = reduce(
-        lambda acc, value: flatten(map(lambda previous_sum: (previous_sum + value, previous_sum * value), acc)),
+        lambda acc, value: flatten(map(lambda previous_sum: [operator(previous_sum, value) for operator in operators], acc)),
         equation[1:],
         [equation[0]])
-    possible_equations = reduce(
-        lambda acc, value: flatten(map(lambda previous_equation: (previous_equation + f" + {value}", previous_equation + f" * {value}"), acc)),
-        equation[1:],
-        [str(equation[0])]
-    )
     return test_value in possible_sums
 
 
@@ -22,4 +19,6 @@ if __name__ == "__main__":
     with open("input", "r") as file:
         lines = [line.strip() for line in file.readlines()]
         equations = [(int(line.split(':')[0].strip()), list(map(int, line.split(':')[1].strip().split(' ')))) for line in lines]
-        print(f"part 1: {sum([k for k, v in equations if valid(k, v)])}")
+        print(f"part 1: {sum([k for k, v in equations if valid(k, v, [add, mul])])}")
+
+        print(f"part 2: {sum([k for k, v in equations if valid(k, v, [add, mul, concatenation])])}")
