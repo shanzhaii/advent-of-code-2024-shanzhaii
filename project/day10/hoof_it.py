aab6ef0631ead8ec4092dd7733c5cddb9154e631
rioa <alexandre.riou@secutix.com>
2024-12-10 11:58:18 +0100

    implement part 2

--- project/day10/hoof_it.py
+++ project/day10/hoof_it.py
@@ -9,26 +9,31 @@ def find_all_trailheads(grid):
                 trailheads.append((y,x))
     return trailheads
 
-def find_trailhead_score(head, grid):
-    location_to_score = {head: 0}
-    explored = []
-    score = 0
+def find_trailhead_score_and_rating(head, grid):
+    path_to_explore = [([head], 0)]
+    path_to_end = []
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    while location_to_score:
-        for location, height in dict(location_to_score).items():
+    while path_to_explore:
+        for path_height_tuple in path_to_explore:
+            path, height = path_height_tuple[0], path_height_tuple[1]
             if height == 9:
-                score += 1
+                if path not in path_to_end:
+                    path_to_end.append(path)
             else:
                 for direction in directions:
-                    new_location = (location[0] + direction[0], location[1] + direction[1])
-                    if is_valid(new_location, grid) and new_location not in explored and grid[new_location[0]][new_location[1]] == height + 1:
-                        location_to_score[new_location] = height + 1
-            explored.append(location)
-            location_to_score.pop(location)
-    return score
+                    new_location = (path[-1][0] + direction[0], path[-1][1] + direction[1])
+                    if is_valid(new_location, grid) and grid[new_location[0]][new_location[1]] == height + 1:
+                        new_path = list(path)
+                        new_path.append(new_location)
+                        path_to_explore.append((new_path, height + 1))
+            path_to_explore.remove((path, height))
+    return len(set(map(lambda path: path[-1], path_to_end))), len(path_to_end)
 
 
 if __name__ == "__main__":
     with open("input", "r", newline='\n') as file:
         grid = [list(map(int, line.strip())) for line in file.readlines()]
-        print(f"part 1: {sum(map(lambda trailhead: find_trailhead_score(trailhead, grid), find_all_trailheads(grid)))}")
+        score, rating = zip(*map(lambda trailhead: find_trailhead_score_and_rating(trailhead, grid), find_all_trailheads(grid)))
+        print(f"part 1: {sum(score)}")
+
+        print(f"part 2: {sum(rating)}")
