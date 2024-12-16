3dae83a55fca65e803554b9d4a0a3abc41c0778c
rioa <alexandre.riou@secutix.com>
2024-12-16 14:40:45 +0100

    day 16 part 2 single path implementation

--- project/day16/reindeer_maze.py
+++ project/day16/reindeer_maze.py
@@ -7,6 +7,7 @@ def run(grid):
     start = find_symbol(grid, 'S')
     to_visit = {(start[0], start[1], 1)}
     scores = {(start[0], start[1], 1): 0}
+    paths = {(start[0], start[1], 1): [(start[0], start[1], 1)]}
     while to_visit:
         current_pos = to_visit.pop()
         x, y, direction = current_pos
@@ -14,17 +15,24 @@ def run(grid):
         if grid[in_front[0]][in_front[1]] != '#' and (in_front not in scores or scores[in_front] > scores[current_pos] + 1):
             to_visit.add(in_front)
             scores[in_front] = scores[current_pos] + 1
+            paths[in_front] = paths[current_pos] + [in_front]
         for rotation in [1, -1]:
             new_pos = (x, y, len(directions) + direction + rotation if direction + rotation < 0 else (direction+rotation) % len(directions))
             if new_pos not in scores or scores[new_pos] > scores[current_pos] + 1000:
                 to_visit.add(new_pos)
                 scores[new_pos] = scores[current_pos] + 1000
+                paths[new_pos] = paths[current_pos] + [new_pos]
     y_end, x_end = find_symbol(grid, 'E')
-    end_scores = [scores[end_point] for end_point in [(y_end, x_end, direction) for direction in range(4)]]
-    return min(end_scores)
+    scores, endpoints = zip(*[(scores[endpoint], endpoint) for endpoint in [(y_end, x_end, direction) for direction in range(4)]])
+    score, endpoint = min(scores), endpoints[scores.index(min(scores))]
+    return score, set(map(lambda pos_rot: (pos_rot[0], pos_rot[1]), paths[endpoint]))
 
 
 if __name__ == "__main__":
-    with open("input", "r", newline='\n') as file:
+    with open("test1", "r", newline='\n') as file:
         grid = [list(line.strip()) for line in file.readlines()]
-        print(f"part 1: {run(grid)}")
+        score, path = run(grid)
+        print(f"part 1: {score}")
+
+        print(f"part 2: {len(path)}")
+
