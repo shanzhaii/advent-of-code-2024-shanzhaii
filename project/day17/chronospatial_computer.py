f1e57b108e75fdb9b1090d0d0331a3e89092b52f
rioa <alexandre.riou@secutix.com>
2024-12-17 18:35:29 +0100

    implement day 17 part 2

--- project/day17/chronospatial_computer.py
+++ project/day17/chronospatial_computer.py
@@ -67,11 +67,36 @@ def run_program(program, a, b, c):
     while ptr < len(program):
         a, b, c, ptr = run_instruction(program[ptr], program[ptr+1], ptr, a, b, c, output)
     return ','.join(map(str, output))
+
+def recursively_find_a(program, a, depth):
+        if depth > len(program):
+            return a
         
+        possible_a = run_for_different_a(program, a, program[len(program) - depth:])
+        if not possible_a:
+            return False
+        else:
+            for possible_a_value in possible_a:
+                next_a = recursively_find_a(program, possible_a_value, depth + 1)
+                if next_a:
+                    return next_a
+            return False
+                
+def run_for_different_a(program, a, current_goal_output):
+    possible_a = []
+    for i in range(8):
+        next_a = a*8 + i
+        output = run_program(program, next_a, 0, 0)
+        if output == ','.join(map(str, current_goal_output)):
+            possible_a.append(next_a)
+    return possible_a
+
 
 if __name__ == "__main__":
     with open("input", "r", newline='\n') as file:
         registers, program = file.read().split('\n\n')
         a, b, c = map(lambda register_info: int(register_info.strip('Register ABC:')), registers.split('\n'))
         program = list(map(int, program.strip('Program: ').split(',')))
-        print(run_program(program, a, b, c))
+        print(f"part 1: {run_program(program, a, b, c)}")
+
+        print(f"part 2: {recursively_find_a(program, 0, 1)}")
