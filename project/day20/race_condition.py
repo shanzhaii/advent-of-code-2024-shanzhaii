91ef3aae4c04913a1b810cf3795b34935c17007f
shanzhaiii <alexandre.riou@epfl.ch>
2024-12-22 22:29:58 +0100

    implement day 20 part 1

--- project/day20/race_condition.py
+++ project/day20/race_condition.py
@@ -1,13 +1,15 @@
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 def is_valid(pos, size):
     return 0 <= pos[0] < size[0] and 0 <= pos[1] < size[1]
 
+
 def find_features(grid):
     obstacles = set()
-    size = (len(grid)-2, len(grid[0])-2)
-    for y in range(1, len(grid)-1):
-        for x in range(1, len(grid[0])-1):
+    size = (len(grid) - 2, len(grid[0]) - 2)
+    for y in range(1, len(grid) - 1):
+        for x in range(1, len(grid[0]) - 1):
             value = grid[y][x]
-            pos = (y-1, x-1)
+            pos = (y - 1, x - 1)
             if value == '#':
                 obstacles.add(pos)
             elif value == 'S':
@@ -16,38 +18,42 @@ def find_features(grid):
                 end = pos
     return obstacles, start, end, size
 
-def traverse_maze(obstacles, start, end, size, cheat_time = None):
+
+def traverse_maze(obstacles, start, end, size):
     to_explore = {start}
-    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     cost = {start: 0}
-    cheat_start, cheat_end = (-1, -1), (-1, -1)
+    path = {start: [(start)]}
     while to_explore:
         pos = to_explore.pop()
         for direction in directions:
             in_front = (pos[0] + direction[0], pos[1] + direction[1])
-            if is_valid(in_front, size) and (in_front not in obstacles or (cheat_time is not None and cost[pos] + 1 == cheat_time)) and (in_front not in cost or cost[in_front] >= cost[pos] + 1):
-                if cheat_time is not None:
-                    if in_front in obstacles:
-                        cheat_start = in_front
-                    if pos == cheat_start:
-                        cheat_end = in_front
+            if (is_valid(in_front, size)
+                    and (in_front not in obstacles)
+                    and (in_front not in cost or cost[in_front] > cost[pos] + 1)):
                 to_explore.add(in_front)
                 cost[in_front] = cost[pos] + 1
-    if cheat_time is not None:
-        return cost[end], (cheat_start, cheat_end)
-    return cost[end] if end in cost else False
+                path[in_front] = path[pos] + [in_front]
+    return path[end], cost[end]
+
 
 def find_cheats(grid):
     obstacles, start, end, size = find_features(grid)
-    normal_time = traverse_maze(obstacles, start, end, size)
-    times = {}
+    # cheats = {}
+    cheat_count = 0
+    normal_path, normal_time = traverse_maze(obstacles, start, end, size)
+    for i, step in enumerate(normal_path):
+        for direction in directions:
+            in_front = (step[0] + direction[0], step[1] + direction[1])
+            if is_valid(in_front, size) and (in_front in obstacles):
+                for next_direction in directions:
+                    next_in_front = (in_front[0] + next_direction[0], in_front[1] + next_direction[1])
+                    if next_in_front in normal_path and normal_path.index(next_in_front) - i - 2 >= 100:
+                        cheat_count += 1
+    return cheat_count
 
-    for i in range(normal_time):
-        cheated_time, cheat = traverse_maze(obstacles, start, end, size, i)
-        times.setdefault(cheated_time, set()).add(cheat)
-    print(times)
+
 
 if __name__ == "__main__":
-    with open("test", "r", newline='\n') as file:
+    with open("input", "r", newline='\n') as file:
         grid = [list(line.strip()) for line in file.readlines()]
-        find_cheats(grid)
+        print(find_cheats(grid))
