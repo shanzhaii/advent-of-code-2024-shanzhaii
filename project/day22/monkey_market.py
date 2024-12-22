ad3cd8e192599e541ca5533d4db7233e9460c232
shanzhaiii <alexandre.riou@epfl.ch>
2024-12-22 13:49:30 +0100

    implement prices and changes calculator

--- project/day22/monkey_market.py
+++ project/day22/monkey_market.py
@@ -17,8 +17,21 @@ def calculate_nth(secret_number, n):
         secret_number = calculate(secret_number)
     return secret_number
 
+def calculate_prices_and_changes(secret_number, n):
+    price = int(str(secret_number)[-1])
+    prices = [price]
+    changes = []
+    last_price = price
+    for _ in range(n):
+        secret_number = calculate(secret_number)
+        price = int(str(secret_number)[-1])
+        prices.append(price)
+        changes.append(price - last_price)
+        last_price = price
+    return prices, changes
 
 if __name__ == "__main__":
     with open("input", "r", newline='\n') as file:
         input = list(map(lambda line: int(line.strip()), file.readlines()))
-        print(f"part 1: {sum(map(lambda secret_number: calculate_nth(secret_number, 2000), input))}")
+        # print(f"part 1: {sum(map(lambda secret_number: calculate_nth(secret_number, 2000), input))}")
+        print(calculate_prices_and_changes(123, 10))
