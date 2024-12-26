e1a08e6809c705bfdaf21d999a9ccb8dc3a0d5ec
shanzhaiii <alexandre.riou@epfl.ch>
2024-12-27 00:41:03 +0100

    implement computation saving day 20

--- project/day20/race_condition.py
+++ project/day20/race_condition.py
@@ -22,24 +22,30 @@ def find_features(grid):
 def traverse_maze(obstacles, start, end, size, max_cost=None, calculate_path=False):
     to_explore = [start]
     explored = set()
-    cost = {start: 0}
-    path = {start: [(start)]}
-    while to_explore and (max_cost is None or cost[to_explore[0]] <= max_cost):
-        pos = to_explore.pop(0)
-        if pos == end:
+    cost, path = storage.get(max_cost, ({},{}))
+    cost.setdefault((start, start), 0)
+    path.setdefault((start, start), [start])
+    while to_explore and (max_cost is None or cost[(start, to_explore[0])] <= max_cost):
+        if (start, end) in cost:
             if calculate_path:
-                return path[end], cost[end]
-            return cost[end]
+                return path[(start, end)], cost[(start, end)]
+            return cost[(start, end)]
+        pos = to_explore.pop(0)
+        if (start, pos) in cost and (pos, end) in cost: # first part should be trivial; can replace initial statement
+            if calculate_path:
+                return path[(start, pos)] + path[(pos, end)], cost[(start, pos)] + cost[(pos, end)]
+            return cost[(start, pos)] + cost[(pos, end)]
         for direction in directions:
             in_front = (pos[0] + direction[0], pos[1] + direction[1])
             if (is_valid(in_front, size)
                     and (in_front not in obstacles)
-                    and (in_front not in cost or cost[in_front] > cost[pos] + 1)):
+                    and ((start, in_front) not in cost or cost[(start, in_front)] > cost[(start, pos)] + 1)):
                 if in_front not in to_explore or in_front not in explored:
                     to_explore.append(in_front)
-                cost[in_front] = cost[pos] + 1
-                if calculate_path:
-                    path[in_front] = path[pos] + [in_front]
+                full_previous_path = path[(start, pos)]
+                for i in range(len(full_previous_path)):
+                    path[(full_previous_path[i], in_front)] = full_previous_path[i:] + [in_front]
+                    cost[(full_previous_path[i], in_front)] = len(full_previous_path) - i
         explored.add(pos)
     return None
 
@@ -69,11 +75,11 @@ def find_cheats(grid, max_cheat_cost, min_cheat):
                     explored_cheats.add(cheat)
     return sum([len(positions) for cheat_save, positions in cheats.items() if cheat_save >= min_cheat])
 
-
+storage = {}
 
 if __name__ == "__main__":
-    with open("input", "r", newline='\n') as file:
+    with open("test", "r", newline='\n') as file:
         grid = [list(line.strip()) for line in file.readlines()]
-        print(f"part 1: {find_cheats(grid, 2, 100)}")
+        print(f"part 1: {find_cheats(grid, 2, 0)}")
 
         print(f"part 2: {find_cheats(grid, 20, 100)}")
